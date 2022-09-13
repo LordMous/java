e7780e8c14f45b5587dde49e3f8c5ea847eed529
LordMous <90604774+LordMous@users.noreply.github.com>
2022-09-13 09:10:53 +0200

    Add files via upload

---
+++ JavaGyak01.java
@@ -0,0 +1,24 @@
+package javagyak01;
+
+public class JavaGyak01 {
+
+    public static void main(String[] args) {
+        
+        int a = 010;
+        System.out.print(a);
+        System.out.println("\t Hello \n Worl \b d");
+        int b = 0x11;
+        System.out.println(a+b);
+        int fokt = 015;
+        System.out.println(fokt);
+        int helyes = 0x2e;
+        System.out.println(helyes);
+        int faLakas;
+        faLakas = 22;
+        int harmincot = 0x23;
+        int szaz = 0x64;
+        System.out.println(szaz);
+    }
+    
+}
+
---
+++ JavaGyak2.java
@@ -0,0 +1,19 @@
+package javagyak2;
+
+public class JavaGyak2 {
+
+    /*Csatlos Szabolcs
+    kedvenc tantargyam az asztali alkalmazasok fejlesztese mert imadom a javat*/
+    
+    public static void main(String[] args) {
+        int ElsoValtozonk;
+        int mv = 0;
+        ElsoValtozonk=6;
+        //System.out.println("Elsovaltozonk :"+ElsoValtozonk*3);
+        for(int i=-6; i<12; i++){
+            System.out.println(i+ElsoValtozonk);
+                   
+      
+    }   
+}
+}
