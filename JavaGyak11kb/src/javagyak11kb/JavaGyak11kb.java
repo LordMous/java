9ce449bdf26f7e5707560831be3a5b623786de46
LordMous <90604774+LordMous@users.noreply.github.com>
2022-10-03 11:09:39 +0200

    Add files via upload

---
+++ JavaGyak11kb/src/javagyak11kb/JavaGyak11kb.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javagyak11kb;
+
+/**
+ *
+ * @author 20c_csatloss
+ */
+public class JavaGyak11kb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        /*
+        int a = 3, b = 4;
+        System.out.println((a++ * b + 5) % 4 > 1 || a > b);
+        jo lett
+        */
+        
+        int a = 4, b = 4;
+        System.out.println(--a * b-- << 2 % 3 > 10 && a == b);
+    }
+    
+}
---
+++ Javagyakvalahany/src/javagyakvalahany/Javagyakvalahany.java
@@ -0,0 +1,29 @@
+package javagyakvalahany;
+
+public class Javagyakvalahany {
+
+    public static void main(String[] args) {
+        String s1 = " ";
+        String s2 = new String("Masodik string");
+        //System.out.println(s1.charAt(5));
+        //System.out.println(s1.concat(" es meg ez is"));
+        //System.out.println(s1.contains("a"));
+        //System.out.println(s1.endsWith("ng"));
+        //System.out.println(s1.isEmpty());
+        //System.out.println(s2.indexOf("dik"));
+        //System.out.println(s1.isBlank());
+        //System.out.println(s2.lastIndexOf("i"));
+        //int a = s2.lastIndexOf("i");
+        //System.out.println(s2.charAt(--a));
+        //System.out.println(s2.replace('i', 'e'));
+        //System.out.println(s1.toUpperCase());
+        //System.out.println(s2.toLowerCase());
+        //utolso n betuje utani karakter
+        //int a = s2.lastIndexOf("n");
+        //System.out.println(++a);
+        //elso s betu elotti karakter
+        //System.out.println(s2.lastIndexOf("s")-1);
+        //System.out.println(s1.isEmpty());
+    }
+    
+}
