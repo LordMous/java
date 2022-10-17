a661df2701111c0e56226e49823c51c741be25b8
LordMous <90604774+LordMous@users.noreply.github.com>
2022-10-17 10:54:14 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,102 +1,52 @@
 package org.example;
 
 import java.util.Scanner;
-//import javax.swing.JOptionPane;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         /*
-        String s = sc.nextLine();
-        char a = sc.next().charAt(0);
-        System.out.println(String.valueOf(a).concat(s));
+        //1,feladat
+        float f1 = sc.nextFloat();
+        float f2 = sc.nextFloat();
+        System.out.println((f1 > f2) ? f1 : f2);
 
+        //2.feladat
         int i1 = sc.nextInt();
         int i2 = sc.nextInt();
-        System.out.println(i1+""+i2);
+        System.out.println(Math.pow(i1,i2));
 
-        String s = sc.nextLine();
-        System.out.println(s.substring(0, s.length()/2));
-        System.out.println(s.substring(s.length()/2, s.length()));
+        //3.feladat
+        System.out.println(Math.sqrt(i2));
 
-        String s =sc.nextLine();
-        char a = sc.next().charAt(0);
-        System.out.println(s.endsWith(String.valueOf(a)));
-
-        String szo = "Szabolcs ";
-        System.out.println(!szo.isBlank()+" "+szo.isEmpty());
-
-
-        short s1 = sc.nextShort();
-        short s2 = sc.nextShort();
-        System.out.println(s1+1+","+(s2+1));
-
-        byte b1 = sc.nextByte();
-        byte b2 = sc.nextByte();
-        System.out.println(b1 ^ b2);
-
-        String s = sc.nextLine();
-        char a = sc.next().charAt(0);
-        System.out.println(s.replace(String.valueOf(a), String.valueOf(a).toUpperCase()));
-
-
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        System.out.println(s1.charAt(s1.length()-1) == s2.charAt(s1.length()-1) ? "Megegyezik" : "Nem egyezik");
-
-
-        double sugar = sc.nextDouble();
-        System.out.println("A kor kerulete :"+""+2*sugar*Math.PI);
-        System.out.println("A kor terulete :"+""+Math.pow(sugar,2)*Math.PI);
-
-
-        int i = sc.nextInt();
+        //4.feladat
+        int i = -120;
         System.out.println(Math.abs(i));
-        System.out.println((i < 0) ? i*-1 : i);
+
+        //5.feladat
+        System.out.println(Math.floor(Math.random()*90+1));
+
+        //6.feladat
+        System.out.println(Math.floor(Math.random()*6)+5);
+
+        //7.feladat
+        System.out.println(Math.ceil(Math.random()*10)+20);
 
 
-        System.out.println(Math.ceil(5.01));
 
+        //8.feladat
+        System.out.println(Math.round(Math.random()*21)-11);
 
-        System.out.println(Math.cos(0));
-
-
-        System.out.println(Math.floor(5.9));
-
-        int i1 = sc.nextInt();
-        int i2 = sc.nextInt();
-        System.out.println(Math.max(i1, i2));
-        System.out.println(i1 > i2 ? i1 : i2);
-
-
-        int i1 = sc.nextInt();
-        int i2 = sc.nextInt();
-        System.out.println(Math.min(i1, i2));
-        System.out.println(i1 > i2 ? i2 : i1);
-
-
-        int i1 = sc.nextInt();
-        int i2 = sc.nextInt();
-        System.out.println(Math.pow(i1, i2));
-
-        System.out.println(Math.round());
-        System.out.println(Math.floor(Math.random()*10+1));
-
-
-        System.out.println(Math.sqrt(9));
 
         int i1 = sc.nextInt();
         int i2 = sc.nextInt();
         System.out.println(Math.abs(i1) > Math.abs(i2) ? i1 : i2);
-
-
-        double d1 = Math.floor(Math.random()*100)+50;
-        double d2 = Math.floor(Math.random()*100)+50;
-        System.out.println(d1+" "+d2);
         */
+        for(int i = 1; i < 20; i++){
+            System.out.println(Math.round(Math.random()*21)-11);
+        }
 
-        int i = sc.nextInt();
-        System.out.println(Math.sqrt(i));
+
+
     }
 }
